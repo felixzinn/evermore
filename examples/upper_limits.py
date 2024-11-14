1faac6726606ba3d0658afb46756abad4ff02841
Felix Zinn <felix.zinn@rwth-aachen.de>
2024-11-14 18:03:00 +0100

    add pmu, upper limit calculation

--- examples/upper_limits.py
+++ examples/upper_limits.py
@@ -14,9 +14,6 @@ import evermore as evm
 optim = optax.sgd(learning_rate=1e-2)
 opt_state = optim.init(eqx.filter(model, eqx.is_inexact_array))
 
-# fix mu to 0
-model_fixed_mu = eqx.tree_at(lambda t: t.mu.value, model, jnp.array(0))
-
 
 @eqx.filter_jit
 def loss(dynamic_model, static_model, hists, observation):
@@ -43,7 +40,7 @@ def make_step(model, opt_state, events, observation, filter_spec):
 
 
 @jax.jit
-def unconditional_fit(steps: int = 1000) -> tuple[eqx.Module, tuple]:
+def unconditional_fit(steps: int = 10_000) -> tuple[eqx.Module, tuple]:
     def fit(step, model_optstate):
         model, opt_state = model_optstate
         return make_step(
@@ -54,7 +51,7 @@ def unconditional_fit(steps: int = 1000) -> tuple[eqx.Module, tuple]:
 
 
 @jax.jit
-def conditional_fit(steps: int = 1000) -> tuple[eqx.Module, tuple]:
+def conditional_fit(mu_val: float, steps: int = 10_000) -> tuple[eqx.Module, tuple]:
     def fit(step, model_optstate):
         model, opt_state = model_optstate
 
@@ -67,10 +64,13 @@ def conditional_fit(steps: int = 1000) -> tuple[eqx.Module, tuple]:
 
         return make_step(model, opt_state, hists, observation, filter_spec)
 
+    # fix mu to value
+    model_fixed_mu = eqx.tree_at(lambda t: t.mu.value, model, jnp.array(mu_val))
+
     return jax.lax.fori_loop(0, steps, fit, (model_fixed_mu, opt_state))
 
 
-def likelihood_ratio() -> tuple[float, Array]:
+def likelihood_ratio(mu_val: float = 0.0) -> tuple[float, Array]:
     """
     Likelihood ratio
     lambda (mu) = L(mu, theta_hat_hat) / L(mu_hat, theta_hat)
@@ -78,7 +78,7 @@ def likelihood_ratio() -> tuple[float, Array]:
     return - 2 * ln(lambda ( 0 ))
     """
     model_unconditional, _ = unconditional_fit()
-    model_conditional, _ = conditional_fit()
+    model_conditional, _ = conditional_fit(mu_val=mu_val)
 
     nll_conditional = loss(
         *eqx.partition(
@@ -113,3 +113,15 @@ p0 = 1 - jsp.stats.norm.cdf(jnp.sqrt(q0))
 
 # https://github.com/scikit-hep/pyhf/blob/3d26434be836050e334190c212b1db1a6f7650c8/src/pyhf/infer/calculators.py#L170
 p0_alt = jsp.stats.norm.cdf(-jnp.sqrt(q0))
+
+# q_mu for upper limit
+mu = 0.0
+qmu = jnp.where(mu_hat <= mu, likelihood_ratio_value, 0.0)
+pmu = 1 - jsp.stats.norm.cdf(jnp.sqrt(qmu))
+
+mu_vals = jnp.arange(0, 10, 0.5)
+pmu_vals = []
+for mu in mu_vals:
+    lr_val, mu_hat = likelihood_ratio(mu)
+    qmu = jnp.where(mu_hat <= mu, lr_val, 0.0)
+    pmu_vals.append(1 - jsp.stats.norm.cdf(jnp.sqrt(qmu)))
